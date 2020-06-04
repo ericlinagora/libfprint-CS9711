iafilatov/libfprint

commit 9fb789dc787dbaa7caa0bc7dd2b9a1baa1f9a750

nbis: Update to NBIS 5.0.0
This updates NBIS to its latest 5.0.0 version, dated 04/03/2015, from a
1.x version, dated 2007.

Original sources are available at:
https://www.nist.gov/itl/iad/image-group/products-and-services/image-group-open-source-server-nigos#Releases

And full change log at:
https://www.nist.gov/sites/default/files/documents/2016/12/14/changelog.txt

libfprint/nbis/mindtct/globals.c
@@ -1,25 +1,46 @@
 /*******************************************************************************
 
-License: 
-This software was developed at the National Institute of Standards and 
-Technology (NIST) by employees of the Federal Government in the course 
-of their official duties. Pursuant to title 17 Section 105 of the 
-United States Code, this software is not subject to copyright protection 
-and is in the public domain. NIST assumes no responsibility  whatsoever for 
-its use by other parties, and makes no guarantees, expressed or implied, 
-about its quality, reliability, or any other characteristic. 
+License:
+This software and/or related materials was developed at the National Institute
+of Standards and Technology (NIST) by employees of the Federal Government
+in the course of their official duties. Pursuant to title 17 Section 105
+of the United States Code, this software is not subject to copyright
+protection and is in the public domain.
 
-Disclaimer: 
-This software was developed to promote biometric standards and biometric
-technology testing for the Federal Government in accordance with the USA
-PATRIOT Act and the Enhanced Border Security and Visa Entry Reform Act.
-Specific hardware and software products identified in this software were used
-in order to perform the software development.  In no case does such
-identification imply recommendation or endorsement by the National Institute
-of Standards and Technology, nor does it imply that the products and equipment
-identified are necessarily the best available for the purpose.  
+This software and/or related materials have been determined to be not subject
+to the EAR (see Part 734.3 of the EAR for exact details) because it is
+a publicly available technology and software, and is freely distributed
+to any interested party with no licensing requirements.  Therefore, it is
+permissible to distribute this software as a free download from the internet.
+
+Disclaimer:
+This software and/or related materials was developed to promote biometric
+standards and biometric technology testing for the Federal Government
+in accordance with the USA PATRIOT Act and the Enhanced Border Security
+and Visa Entry Reform Act. Specific hardware and software products identified
+in this software were used in order to perform the software development.
+In no case does such identification imply recommendation or endorsement
+by the National Institute of Standards and Technology, nor does it imply that
+the products and equipment identified are necessarily the best available
+for the purpose.
+
+This software and/or related materials are provided "AS-IS" without warranty
+of any kind including NO WARRANTY OF PERFORMANCE, MERCHANTABILITY,
+NO WARRANTY OF NON-INFRINGEMENT OF ANY 3RD PARTY INTELLECTUAL PROPERTY
+or FITNESS FOR A PARTICULAR PURPOSE or for any purpose whatsoever, for the
+licensed product, however used. In no event shall NIST be liable for any
+damages and/or costs, including but not limited to incidental or consequential
+damages of any kind, including economic damage or injury to property and lost
+profits, regardless of whether NIST shall be advised, have reason to know,
+or in fact shall know of the possibility.
+
+By using this software, you agree to bear all risk relating to quality,
+use and performance of the software and/or related materials.  You agree
+to hold the Government harmless from any claim arising from your use
+of the software.
 
 *******************************************************************************/
+
 
 /***********************************************************************
       LIBRARY: LFS - NIST Latent Fingerprint System
@@ -40,7 +61,7 @@ identified are necessarily the best available for the purpose.
 /*************************************************************************/
 
 #ifdef LOG_REPORT
-FILE *g_logfp;
+FILE *logfp;
 #endif
 
 /* Constants (C) for defining 4 DFT frequencies, where  */
@@ -129,8 +150,6 @@ LFSPARMS g_lfsparms = {
    PORES_STEPS_BWD,
    PORES_MIN_DIST2,
    PORES_MAX_RATIO,
-   FALSE, /* not removing perimeter points by default */
-   PERIMETER_PTS_DISTANCE,
 
    /* Ridge Counting Controls */
    MAX_NBRS,
@@ -215,8 +234,6 @@ LFSPARMS g_lfsparms_V2 = {
    PORES_STEPS_BWD,
    PORES_MIN_DIST2,
    PORES_MAX_RATIO,
-   FALSE, /* not removing perimeter points by default */
-   PERIMETER_PTS_DISTANCE,
 
    /* Ridge Counting Controls */
    MAX_NBRS,

libfprint/meson.build
@@ -30,14 +30,17 @@ nbis_sources = [
     'nbis/bozorth3/bz_sort.c',
     'nbis/mindtct/binar.c',
     'nbis/mindtct/block.c',
+    'nbis/mindtct/chaincod.c',
     'nbis/mindtct/contour.c',
     'nbis/mindtct/detect.c',
     'nbis/mindtct/dft.c',
     'nbis/mindtct/free.c',
+    'nbis/mindtct/getmin.c',
     'nbis/mindtct/globals.c',
     'nbis/mindtct/imgutil.c',
     'nbis/mindtct/init.c',
     'nbis/mindtct/line.c',
+    'nbis/mindtct/link.c',
     'nbis/mindtct/log.c',
     'nbis/mindtct/loop.c',
     'nbis/mindtct/maps.c',
@@ -50,6 +53,7 @@ nbis_sources = [
     'nbis/mindtct/shape.c',
     'nbis/mindtct/sort.c',
     'nbis/mindtct/util.c',
+    'nbis/mindtct/xytreps.c',
 ]
 
 aeslib = false

ericlinagora/libfprint-CS9711

commit 297236b51a537d56b0a1e425d3e8582403f5ecb2

nbis: re-add step to remove perimeter points
This step helps to drop false minutiae for short sensors and
it was accidentally dropped during NBIS update. Re-add this step
and add a patch to update script to ensure that it's not dropped
during next update.

Fixes: 9fb789dc787d ("nbis: Update to NBIS 5.0.0")

libfprint/nbis/include/lfs.h
@@ -260,6 +260,8 @@ typedef struct g_lfsparms{
    int    pores_steps_bwd;
    double pores_min_dist2;
    double pores_max_ratio;
+   int    remove_perimeter_pts;
+   int    min_pp_distance;
 
    /* Ridge Counting Controls */
    int    max_nbrs;
@@ -608,6 +610,9 @@ typedef struct g_lfsparms{
 /* Max ratio of computed distances between pairs of forward and backward */
 /* contour points to be considered a pore.                               */
 #define PORES_MAX_RATIO          2.25
+
+/* Points which are closer than this distance to scan perimeter will be removed */
+#define PERIMETER_PTS_DISTANCE 10
 
 
 /***** RIDGE COUNTING CONSTANTS *****/
@@ -1123,6 +1128,9 @@ extern int remove_or_adjust_side_minutiae(MINUTIAE *, unsigned char *,
 extern int remove_or_adjust_side_minutiae_V2(MINUTIAE *,
                   unsigned char *, const int, const int,
                   int *, const int, const int, const LFSPARMS *);
+extern int remove_perimeter_pts(MINUTIAE *minutiae,
+                  unsigned char *bdata, const int iw, const int ih,
+                  const LFSPARMS *lfsparms);
 
 /* results.c */
 extern int write_text_results(char *, const int, const int, const int,

libfprint/nbis/mindtct/globals.c
@@ -150,6 +150,8 @@ LFSPARMS g_lfsparms = {
    PORES_STEPS_BWD,
    PORES_MIN_DIST2,
    PORES_MAX_RATIO,
+   FALSE, /* not removing perimeter points by default */
+   PERIMETER_PTS_DISTANCE,
 
    /* Ridge Counting Controls */
    MAX_NBRS,
@@ -234,6 +236,8 @@ LFSPARMS g_lfsparms_V2 = {
    PORES_STEPS_BWD,
    PORES_MIN_DIST2,
    PORES_MAX_RATIO,
+   FALSE, /* not removing perimeter points by default */
+   PERIMETER_PTS_DISTANCE,
 
    /* Ridge Counting Controls */
    MAX_NBRS,

libfprint/nbis/mindtct/remove.c
@@ -195,6 +195,11 @@ int remove_false_minutia_V2(MINUTIAE *minutiae,
       return(ret);
    }
 
+   /* 11. Remove minutiae on image edge */
+   if((ret = remove_perimeter_pts(minutiae, bdata, iw, ih, lfsparms))) {
+      return (ret);
+   }
+
    return(0);
 }
 
@@ -1327,6 +1332,159 @@ int remove_pointing_invblock_V2(MINUTIAE *minutiae,
 
    /* Return normally. */
    return(0);
+}
+
+static void mark_minutiae_in_range(MINUTIAE *minutiae, int *to_remove, int x, int y,
+                                   const LFSPARMS *lfsparms)
+{
+    int i, dist;
+    for (i = 0; i < minutiae->num; i++) {
+        if (to_remove[i])
+            continue;
+        dist = (int)sqrt((x - minutiae->list[i]->x) * (x - minutiae->list[i]->x) +
+                         (y - minutiae->list[i]->y) * (y - minutiae->list[i]->y));
+        if (dist < lfsparms->min_pp_distance) {
+            to_remove[i] = 1;
+        }
+    }
+}
+
+/*************************************************************************
+**************************************************************************
+#cat: remove_perimeter_pts - Takes a list of true and false minutiae and
+#cat:                attempts to detect and remove those false minutiae that
+#cat:                belong to image edge
+
+   Input:
+      minutiae  - list of true and false minutiae
+      bdata     - binary image data (0==while & 1==black)
+      iw        - width (in pixels) of image
+      ih        - height (in pixels) of image
+      lfsparms  - parameters and thresholds for controlling LFS
+   Output:
+      minutiae  - list of pruned minutiae
+   Return Code:
+      Zero     - successful completion
+      Negative - system error
+**************************************************************************/
+int remove_perimeter_pts(MINUTIAE *minutiae,
+                       unsigned char *bdata, const int iw, const int ih,
+                       const LFSPARMS *lfsparms)
+{
+    int i, j, ret, *to_remove;
+    int *left, *left_up, *left_down;
+    int *right, *right_up, *right_down;
+    int removed = 0;
+    int left_min, right_max;
+
+    if (!lfsparms->remove_perimeter_pts)
+        return(0);
+
+    to_remove = calloc(minutiae->num, sizeof(int));
+    left = calloc(ih, sizeof(int));
+    left_up = calloc(ih, sizeof(int));
+    left_down = calloc(ih, sizeof(int));
+    right = calloc(ih, sizeof(int));
+    right_up = calloc(ih, sizeof(int));
+    right_down = calloc(ih, sizeof(int));
+
+    /* Pass downwards */
+    left_min = iw - 1;
+    right_max = 0;
+    for (i = 0; i < ih; i++) {
+        for (j = 0; j < left_min; j++) {
+            if ((bdata[i * iw + j] != 0)) {
+                left_min = j;
+                break;
+            }
+        }
+        if (left_min == (iw - 1))
+            left_down[i] = -1;
+        else
+            left_down[i] = left_min;
+        for (j = iw - 1; j >= right_max; j--) {
+            if ((bdata[i * iw + j] != 0)) {
+                right_max = j;
+                break;
+            }
+        }
+        if (right_max == 0)
+            right_down[i] = -1;
+        else
+            right_down[i] = right_max;
+    }
+
+    /* Pass upwards */
+    left_min = iw - 1;
+    right_max = 0;
+    for (i = ih - 1; i >= 0; i--) {
+        for (j = 0; j < left_min; j++) {
+            if ((bdata[i * iw + j] != 0)) {
+                left_min = j;
+                break;
+            }
+        }
+        if (left_min == (iw - 1))
+            left_up[i] = -1;
+        else
+            left_up[i] = left_min;
+        for (j = iw - 1; j >= right_max; j--) {
+            if ((bdata[i * iw + j] != 0)) {
+                right_max = j;
+                break;
+            }
+        }
+        if (right_max == 0)
+            right_up[i] = -1;
+        else
+            right_up[i] = right_max;
+    }
+
+    /* Merge */
+    left_min = left_down[ih - 1];
+    right_max = right_down[ih - 1];
+    for (i = 0; i < ih; i++) {
+        if (left_down[i] != left_min)
+            left[i] = left_down[i];
+        else
+            left[i] = left_up[i];
+
+        if (right_down[i] != right_max)
+            right[i] = right_down[i];
+        else
+            right[i] = right_up[i];
+    }
+    free(left_up);
+    free(left_down);
+    free(right_up);
+    free(right_down);
+
+    /* Mark minitiae close to the edge */
+    for (i = 0; i < ih; i++) {
+        if (left[i] != -1)
+            mark_minutiae_in_range(minutiae, to_remove, left[i], i, lfsparms);
+        if (right[i] != -1)
+            mark_minutiae_in_range(minutiae, to_remove, right[i], i, lfsparms);
+    }
+
+    free(left);
+    free(right);
+
+    for (i = minutiae->num - 1; i >= 0; i--) {
+        /* If the current minutia index is flagged for removal ... */
+        if (to_remove[i]){
+            removed ++;
+            /* Remove the minutia from the minutiae list. */
+            if((ret = remove_minutia(i, minutiae))){
+                free(to_remove);
+                return(ret);
+            }
+        }
+    }
+
+    free(to_remove);
+
+    return (0);
 }
 
 /*************************************************************************

libfprint/nbis/remove-perimeter-pts.patch
@@ -1,0 +1,231 @@
+diff --git nbis/include/lfs.h nbis/include/lfs.h
+index f4f38d7..8b12e73 100644
+--- nbis/include/lfs.h
++++ nbis/include/lfs.h
+@@ -260,6 +260,8 @@ typedef struct g_lfsparms{
+    int    pores_steps_bwd;
+    double pores_min_dist2;
+    double pores_max_ratio;
++   int    remove_perimeter_pts;
++   int    min_pp_distance;
+ 
+    /* Ridge Counting Controls */
+    int    max_nbrs;
+@@ -609,6 +611,9 @@ typedef struct g_lfsparms{
+ /* contour points to be considered a pore.                               */
+ #define PORES_MAX_RATIO          2.25
+ 
++/* Points which are closer than this distance to scan perimeter will be removed */
++#define PERIMETER_PTS_DISTANCE 10
++
+ 
+ /***** RIDGE COUNTING CONSTANTS *****/
+ 
+@@ -1123,6 +1128,9 @@ extern int remove_or_adjust_side_minutiae(MINUTIAE *, unsigned char *,
+ extern int remove_or_adjust_side_minutiae_V2(MINUTIAE *,
+                   unsigned char *, const int, const int,
+                   int *, const int, const int, const LFSPARMS *);
++extern int remove_perimeter_pts(MINUTIAE *minutiae,
++                  unsigned char *bdata, const int iw, const int ih,
++                  const LFSPARMS *lfsparms);
+ 
+ /* results.c */
+ extern int write_text_results(char *, const int, const int, const int,
+diff --git nbis/mindtct/globals.c nbis/mindtct/globals.c
+index da10c15..79bc583 100644
+--- nbis/mindtct/globals.c
++++ nbis/mindtct/globals.c
+@@ -150,6 +150,8 @@ LFSPARMS g_lfsparms = {
+    PORES_STEPS_BWD,
+    PORES_MIN_DIST2,
+    PORES_MAX_RATIO,
++   FALSE, /* not removing perimeter points by default */
++   PERIMETER_PTS_DISTANCE,
+ 
+    /* Ridge Counting Controls */
+    MAX_NBRS,
+@@ -234,6 +236,8 @@ LFSPARMS g_lfsparms_V2 = {
+    PORES_STEPS_BWD,
+    PORES_MIN_DIST2,
+    PORES_MAX_RATIO,
++   FALSE, /* not removing perimeter points by default */
++   PERIMETER_PTS_DISTANCE,
+ 
+    /* Ridge Counting Controls */
+    MAX_NBRS,
+diff --git nbis/mindtct/remove.c nbis/mindtct/remove.c
+index af5ab7d..7311f1c 100644
+--- nbis/mindtct/remove.c
++++ nbis/mindtct/remove.c
+@@ -195,6 +195,11 @@ int remove_false_minutia_V2(MINUTIAE *minutiae,
+       return(ret);
+    }
+ 
++   /* 11. Remove minutiae on image edge */
++   if((ret = remove_perimeter_pts(minutiae, bdata, iw, ih, lfsparms))) {
++      return (ret);
++   }
++
+    return(0);
+ }
+ 
+@@ -1329,6 +1334,159 @@ int remove_pointing_invblock_V2(MINUTIAE *minutiae,
+    return(0);
+ }
+ 
++static void mark_minutiae_in_range(MINUTIAE *minutiae, int *to_remove, int x, int y,
++                                   const LFSPARMS *lfsparms)
++{
++    int i, dist;
++    for (i = 0; i < minutiae->num; i++) {
++        if (to_remove[i])
++            continue;
++        dist = (int)sqrt((x - minutiae->list[i]->x) * (x - minutiae->list[i]->x) +
++                         (y - minutiae->list[i]->y) * (y - minutiae->list[i]->y));
++        if (dist < lfsparms->min_pp_distance) {
++            to_remove[i] = 1;
++        }
++    }
++}
++
++/*************************************************************************
++**************************************************************************
++#cat: remove_perimeter_pts - Takes a list of true and false minutiae and
++#cat:                attempts to detect and remove those false minutiae that
++#cat:                belong to image edge
++
++   Input:
++      minutiae  - list of true and false minutiae
++      bdata     - binary image data (0==while & 1==black)
++      iw        - width (in pixels) of image
++      ih        - height (in pixels) of image
++      lfsparms  - parameters and thresholds for controlling LFS
++   Output:
++      minutiae  - list of pruned minutiae
++   Return Code:
++      Zero     - successful completion
++      Negative - system error
++**************************************************************************/
++int remove_perimeter_pts(MINUTIAE *minutiae,
++                       unsigned char *bdata, const int iw, const int ih,
++                       const LFSPARMS *lfsparms)
++{
++    int i, j, ret, *to_remove;
++    int *left, *left_up, *left_down;
++    int *right, *right_up, *right_down;
++    int removed = 0;
++    int left_min, right_max;
++
++    if (!lfsparms->remove_perimeter_pts)
++        return(0);
++
++    to_remove = calloc(minutiae->num, sizeof(int));
++    left = calloc(ih, sizeof(int));
++    left_up = calloc(ih, sizeof(int));
++    left_down = calloc(ih, sizeof(int));
++    right = calloc(ih, sizeof(int));
++    right_up = calloc(ih, sizeof(int));
++    right_down = calloc(ih, sizeof(int));
++
++    /* Pass downwards */
++    left_min = iw - 1;
++    right_max = 0;
++    for (i = 0; i < ih; i++) {
++        for (j = 0; j < left_min; j++) {
++            if ((bdata[i * iw + j] != 0)) {
++                left_min = j;
++                break;
++            }
++        }
++        if (left_min == (iw - 1))
++            left_down[i] = -1;
++        else
++            left_down[i] = left_min;
++        for (j = iw - 1; j >= right_max; j--) {
++            if ((bdata[i * iw + j] != 0)) {
++                right_max = j;
++                break;
++            }
++        }
++        if (right_max == 0)
++            right_down[i] = -1;
++        else
++            right_down[i] = right_max;
++    }
++
++    /* Pass upwards */
++    left_min = iw - 1;
++    right_max = 0;
++    for (i = ih - 1; i >= 0; i--) {
++        for (j = 0; j < left_min; j++) {
++            if ((bdata[i * iw + j] != 0)) {
++                left_min = j;
++                break;
++            }
++        }
++        if (left_min == (iw - 1))
++            left_up[i] = -1;
++        else
++            left_up[i] = left_min;
++        for (j = iw - 1; j >= right_max; j--) {
++            if ((bdata[i * iw + j] != 0)) {
++                right_max = j;
++                break;
++            }
++        }
++        if (right_max == 0)
++            right_up[i] = -1;
++        else
++            right_up[i] = right_max;
++    }
++
++    /* Merge */
++    left_min = left_down[ih - 1];
++    right_max = right_down[ih - 1];
++    for (i = 0; i < ih; i++) {
++        if (left_down[i] != left_min)
++            left[i] = left_down[i];
++        else
++            left[i] = left_up[i];
++
++        if (right_down[i] != right_max)
++            right[i] = right_down[i];
++        else
++            right[i] = right_up[i];
++    }
++    free(left_up);
++    free(left_down);
++    free(right_up);
++    free(right_down);
++
++    /* Mark minitiae close to the edge */
++    for (i = 0; i < ih; i++) {
++        if (left[i] != -1)
++            mark_minutiae_in_range(minutiae, to_remove, left[i], i, lfsparms);
++        if (right[i] != -1)
++            mark_minutiae_in_range(minutiae, to_remove, right[i], i, lfsparms);
++    }
++
++    free(left);
++    free(right);
++
++    for (i = minutiae->num - 1; i >= 0; i--) {
++        /* If the current minutia index is flagged for removal ... */
++        if (to_remove[i]){
++            removed ++;
++            /* Remove the minutia from the minutiae list. */
++            if((ret = remove_minutia(i, minutiae))){
++                free(to_remove);
++                return(ret);
++            }
++        }
++    }
++
++    free(to_remove);
++
++    return (0);
++}
++
+ /*************************************************************************
+ **************************************************************************
+ #cat: remove_overlaps - Takes a list of true and false minutiae and

libfprint/nbis/update-from-nbis.sh
@@ -195,3 +195,6 @@ patch -p0 < glib-mem-warning.patch
 
 # Also fix some scan-build warnings, mostly by adding assertions
 patch -p0 < fix-scan-build-reports.patch
+
+# Add pass to remove perimeter points
+patch -p0 < remove-perimeter-pts.patch