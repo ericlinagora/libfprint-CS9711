2cfff27729e9faa12e1e5de3788b431adeb3e7c3
Benjamin Berg <bberg@redhat.com>
2021-11-02 16:27:33 +0100

    udev-hwdb: Update unsupported device list

--- libfprint/fprint-list-udev-hwdb.c
+++ libfprint/fprint-list-udev-hwdb.c
@@ -60,6 +60,7 @@ static const FpIdEntry whitelist_id_table[] = {
   { .vid = 0x0a5c, .pid = 0x5843 },
   { .vid = 0x0a5c, .pid = 0x5844 },
   { .vid = 0x0a5c, .pid = 0x5845 },
+  { .vid = 0x0bda, .pid = 0x5812 },
   { .vid = 0x10a5, .pid = 0x0007 },
   { .vid = 0x1188, .pid = 0x9545 },
   { .vid = 0x138a, .pid = 0x0007 },
