a5e3f3a333aa5a42b9d4d5d3ed8c910ee6ede66e
Natasha England-Elbro <natasha@natashaee.me>
2022-11-05 00:55:45 +0000

    chore: add/update license headers

--- libfprint/sigfm/tests-embedded.hpp
+++ libfprint/sigfm/tests-embedded.hpp
@@ -1,4 +1,12 @@
 
+// SIGFM algorithm for libfprint
+
+// Copyright (C) 2022 Matthieu CHARETTE <matthieu.charette@gmail.com>
+// Copyright (c) 2022 Natasha England-Elbro <natasha@natashaee.me>
+// Copyright (c) 2022 Timur Mangliev <tigrmango@gmail.com>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
 #pragma once
 
 namespace embedded {
--- libfprint/sigfm/tests.cpp
+++ libfprint/sigfm/tests.cpp
@@ -1,3 +1,11 @@
+// SIGFM algorithm for libfprint
+
+// Copyright (C) 2022 Matthieu CHARETTE <matthieu.charette@gmail.com>
+// Copyright (c) 2022 Natasha England-Elbro <natasha@natashaee.me>
+// Copyright (c) 2022 Timur Mangliev <tigrmango@gmail.com>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
 #include "opencv2/core.hpp"
 #include "opencv2/core/types.hpp"
 #include "sigfm.h"
