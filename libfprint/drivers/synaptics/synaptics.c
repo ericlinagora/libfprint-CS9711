3ebd2c3f9745b80d02e5724c5737ff023be04fae
Aris Lin <Aris.Lin@synaptics.com>
2023-10-19 14:25:09 +0800

    synaptics: Add new PID 0x0173

--- libfprint/drivers/synaptics/synaptics.c
+++ libfprint/drivers/synaptics/synaptics.c
@@ -45,6 +45,7 @@ static const FpIdEntry id_table[] = {
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0168,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x015F,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0104,  },
+  { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0173,  },
   { .vid = 0,  .pid = 0,  .driver_data = 0 },   /* terminating entry */
 };
 
